4d2299a75c7b3b938ddd1cb0936f76326613dea1
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-01-21 14:51:10 +0530

    Add network service

---
+++ server-dockui/servcies/network.js
@@ -0,0 +1,58 @@
+const request = require('lib/request')
+
+module.exports = {
+  list,
+  create,
+  read,
+  destroy,
+  prune
+}
+
+async function list(req, res) {
+  try {
+    res.send(await request('', ``))
+  } 
+  catch(error) {
+    res.status(500).send(error)
+  }
+}
+
+async function create(req, res) {
+  try {
+    const body = {
+      name: req.body.name,
+      driver: req.body.driver,
+      labels: req.body.labels
+    }
+    res.send(await request('', ``, body))
+  } 
+  catch(error) {
+    res.status(500).send(error)
+  }
+}
+async function read(req, res) {
+  try {
+    res.send(await request('', ``))
+  } 
+  catch(error) {
+    res.status(500).send(error)
+  }
+}
+
+async function destroy(req, res) {
+  try {
+    res.send(await request('', ``))
+  } 
+  catch(error) {
+    res.status(500).send(error)
+  }
+}
+
+async function prune(req, res) {
+  try {
+    res.send(await request('', ``))
+  } 
+  catch(error) {
+    res.status(500).send(error)
+  }
+}
